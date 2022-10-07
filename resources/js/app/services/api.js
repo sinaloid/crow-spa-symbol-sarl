69fe494739e5d8b10beda4ea85aaf740f8c669f6
Sinali Dionou <sinaloid@gmail.com>
2022-10-07 18:37:40 +0000

    init 1

--- resources/js/app/services/api.js
+++ resources/js/app/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-//const URL = 'https://market-africadefis.herokuapp.com/api/'
-const URL = 'http://market.africadefis.com/api/'
-const URL2 = 'http://127.0.0.1:8000/api/'
-//const URLIMG = 'https://market-africadefis.herokuapp.com/uploads/'
-const URLIMG = 'http://market.africadefis.com/uploads/'
-const URLIMG2 = 'http://127.0.0.1:8000/uploads/'
+const URL = 'https://africadefis.herokuapp.com/api/'
+//const URL = 'http://market.africadefis.com/api/'
+//const URL2 = 'http://127.0.0.1:8000/api/'
+const URLIMG = 'https://africadefis.herokuapp.com/uploads/'
+//const URLIMG = 'http://market.africadefis.com/uploads/'
+//const URLIMG2 = 'http://127.0.0.1:8000/uploads/'
 
 export const urlImg = URLIMG2
 
